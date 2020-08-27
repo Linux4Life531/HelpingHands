257b4240fd015c587b3f202f322ea6344203b32c
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-08-27 15:15:14 +0100

    Fixed Function

--- MariaDBMqsliDateTimeFormating/index.php
+++ MariaDBMqsliDateTimeFormating/index.php
@@ -1,6 +1,6 @@
 function getCurrentDate($databaseName) {
-  if ($databaseName == "MariaDB") {
-    returnData = date("Y-m-d H:i:s");
-  }
-  return returnData;
+    if ($databaseName == "MariaDB") {
+        $returnData = date("Y-m-d H:i:s");
+    }
+    return $returnData;
 }
